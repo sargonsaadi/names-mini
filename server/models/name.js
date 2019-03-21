f4efb969ac5aef5764dfef834318f3e6d4a5fbc8
sargon saadi <sargonsaadi@gmail.com>
2019-03-20 20:40:25 -0700

    updated gitignore and removed folders

--- server/models/name.js
+++ server/models/name.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'password',
+        password: 'password123',
         database: 'namesdb_mini'
     })
 
